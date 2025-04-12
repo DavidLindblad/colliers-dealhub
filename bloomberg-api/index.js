faca435ff5bf42a6623bf4b7e4ef0e30dbadd75e
DavidLindblad <info@salespie.me>
2025-04-12 20:46:34 +0100

    Revert to working version with proper string formatting

--- bloomberg-api/index.js
+++ bloomberg-api/index.js
@@ -25,32 +25,6 @@ const bloombergConfig = {
   snapshotDate: '20250411'
 };
 
-// Equivalent to VBA's APICall function
-async function apiCall(method, body, url, contentType, apiVersion, token, description) {
-  console.log(\Making API call for \...\);
-  console.log('URL:', url);
-  
-  try {
-    const response = await axios({
-      method: method,
-      url: url,
-      data: body,
-      headers: {
-        'Authorization': 'Bearer ' + token,
-        'Accept': 'text/csv',
-        'api-version': apiVersion,
-        ...(contentType && { 'Content-Type': contentType })
-      }
-    });
-    
-    console.log(\\ API call successful\);
-    return response.data;
-  } catch (error) {
-    console.error(\Error in \ API call:\, error.message);
-    throw error;
-  }
-}
-
 async function fetchAndStoreData() {
   console.log('Starting data fetch at:', new Date().toISOString());
   try {
@@ -62,39 +36,51 @@ async function fetchAndStoreData() {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
-      data: \grant_type=client_credentials&client_id=\&client_secret=\\
+      data: 'grant_type=client_credentials&client_id=' + bloombergConfig.clientId + '&client_secret=' + bloombergConfig.clientSecret
     });
 
     const bbToken = tokenResponse.data.access_token;
     console.log('Access token received');
 
-    // Fetch Rates data - exactly matching VBA implementation
+    // Fetch Rates data
     const ratesUrl = bloombergConfig.baseUrl + 
                     'catalogs/' + bloombergConfig.catalog + 
                     '/datasets/' + bloombergConfig.ratesDataset + 
                     '/snapshots/' + bloombergConfig.snapshotDate + 
                     '/distributions/' + bloombergConfig.ratesDataset + '.csv';
 
-    // Equivalent to: BBData1 = APICall("GET", "", reqURL, "", "2", BBToken, "BB Rates")
-    let bbData1 = await apiCall('GET', '', ratesUrl, '', '2', bbToken, 'BB Rates');
-    
-    // Equivalent to: BBData1 = Mid(BBData1, InStr(BBData1, vbLf) + 1)
-    bbData1 = bbData1.substring(bbData1.indexOf('\\n') + 1);
-    console.log('Rates data processed');
+    console.log('Requesting Rates URL:', ratesUrl);
+    const ratesResponse = await axios.get(ratesUrl, {
+      headers: {
+        'Authorization': 'Bearer ' + bbToken,
+        'Accept': 'text/csv',
+        'api-version': '2'
+      }
+    });
 
-    // Fetch CPI data - exactly matching VBA implementation
+    let bbData1 = ratesResponse.data;
+    bbData1 = bbData1.substring(bbData1.indexOf('\\n') + 1);
+    console.log('Rates data received and processed');
+
+    // Fetch CPI data
     const cpiUrl = bloombergConfig.baseUrl + 
                    'catalogs/' + bloombergConfig.catalog + 
                    '/datasets/' + bloombergConfig.cpiDataset + 
                    '/snapshots/' + bloombergConfig.snapshotDate + 
                    '/distributions/' + bloombergConfig.cpiDataset + '.csv';
 
-    // Equivalent to: BBData2 = APICall("GET", "", reqURL, "", "2", BBToken, "BB CPI")
-    let bbData2 = await apiCall('GET', '', cpiUrl, '', '2', bbToken, 'BB CPI');
-    
-    // Equivalent to: BBData2 = Mid(BBData2, InStr(BBData2, vbLf) + 1)
+    console.log('Requesting CPI URL:', cpiUrl);
+    const cpiResponse = await axios.get(cpiUrl, {
+      headers: {
+        'Authorization': 'Bearer ' + bbToken,
+        'Accept': 'text/csv',
+        'api-version': '2'
+      }
+    });
+
+    let bbData2 = cpiResponse.data;
     bbData2 = bbData2.substring(bbData2.indexOf('\\n') + 1);
-    console.log('CPI data processed');
+    console.log('CPI data received and processed');
 
     // Combine and parse the data
     const combinedData = bbData1 + bbData2;
